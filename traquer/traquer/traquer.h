9e7a64638e113c8a0509445d18073508752a1649
Morgan Zellers <mzeller@siue.edu>
2018-09-30 15:17:18 -0500

    Creating initial framework project file

---
+++ traquer/traquer/traquer.h
@@ -0,0 +1,18 @@
+//
+//  traquer.h
+//  traquer
+//
+//  Created by Morgan Zellers on 9/30/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for traquer.
+FOUNDATION_EXPORT double traquerVersionNumber;
+
+//! Project version string for traquer.
+FOUNDATION_EXPORT const unsigned char traquerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <traquer/PublicHeader.h>
+
+
